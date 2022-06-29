49cb05870c113bca07ab80483f3aef8d46f40802
or-orsatti <ororsatti@gmail.com>
2022-06-30 01:35:40 +0300

    added functionality infront of API

---
+++ src/shared/Pokemon.ts
@@ -0,0 +1,66 @@
+export class Pokemon {
+    name: string;
+    weight: number;
+    height: number;
+    img: string;
+    id: number;
+
+    constructor(
+        name: string,
+        weight: number,
+        height: number,
+        img: string,
+        id: number
+    ) {
+        if (weight < 0 || height < 0) {
+            throw new TypeError("weight & height must be bigger then 0");
+        }
+
+        if (!name) {
+            throw new TypeError("can not execpt empty name");
+        }
+
+        if (!img) {
+            throw new TypeError("can not execpt empty image");
+        }
+
+        this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.img = img;
+        this.id = id;
+    }
+}
+/**
+ *
+ * Beaware - async function. means it`ll run seperately if you will not
+ * use it with await.
+ *
+ * this function gets the name of the pokemon and returns a Promise with
+ * the pokemon's object.
+ *
+ */
+export const getPokemonByName = async (pokemonName: string) => {
+    const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
+    const response = await fetch(BASE_URL + pokemonName);
+    const pokemonValues = await response.json();
+
+    const weight = pokemonValues.weight;
+    const height = pokemonValues.height;
+    const name = pokemonValues.name;
+    const id = pokemonValues.id;
+
+    const frontSprite = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+    return new Pokemon(name, weight, height, frontSprite, id);
+};
+
+//##############################
+//Usage
+//##############################
+
+// async function someFunction() {
+//     const ditto = await getPokemonByName("ditto");
+//     console.log(ditto);
+// }
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./dist/tsc/app.js",
+    },
+    devtool: "source-map",
+    output: {
+        filename: "[name].js",
+        library: "app",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                enforce: "pre",
+                use: ["source-map-loader"],
+            },
+        ],
+    },
+};
