2ff697ba76db837ac3c918bc2c61e7fcce85fd7a
or-orsatti <ororsatti@gmail.com>
2022-07-08 17:39:28 +0300

    asyc calls to pokeapi

--- server/script.js
+++ server/script.js
@@ -1,24 +1,42 @@
 const axios = require("axios").default;
 const fs = require("fs");
+
 const fetchTopPokemons = async (n, start = 1) => {
     const allPokemons = [];
-    for (let i = start; i <= n; i++) {
+    for (let i = start; i <= n + start; i++) {
         const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        const fetchedInfo = (await axios.get(url)).data;
-        const { name, id, height, weight } = fetchedInfo;
-        const pokemon = {
-            name,
-            id,
-            height,
-            weight,
-            img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-        };
-        allPokemons.push(pokemon);
-        console.log(`####       loaded pokemon ${name} id:${id}     ####`);
+        axios
+            .get(url)
+            .then((res) => {
+                const { name, id, height, weight } = res.data;
+                const pokemon = {
+                    name,
+                    id,
+                    height,
+                    weight,
+                    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+                };
+                console.log(
+                    `####       loaded pokemon ${name} id:${id} from: ${url}    ####`
+                );
+                allPokemons.push(pokemon);
+                fs.writeFileSync("./data.json", JSON.stringify(allPokemons));
+            })
+            .catch((err) => console.log(`###        ERROR: ${err}     ###`));
     }
     return allPokemons;
 };
 
-fetchTopPokemons(200, 101).then((data) => {
-    fs.appendFileSync("./data.json", JSON.stringify(data));
-});
+// fetchTopPokemons(998, 1);
+
+const getAllIds = () => {
+    const pokemons = JSON.parse(
+        fs.readFileSync("./data.json", { encoding: "utf-8" })
+    );
+
+    return pokemons.map((element) => element.id);
+};
+
+const allIDs = getAllIds();
+console.log(allIDs.reduce((prev, curr) => (prev >= curr ? prev : curr), 1));
+console.log(allIDs.length);
