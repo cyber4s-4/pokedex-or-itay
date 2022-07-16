9cace93a2629764a2e1fd5cd232e3665bbd9a2f6
or-orsatti <ororsatti@gmail.com>
2022-07-16 09:33:24 +0300

    chenged script to run throttling

--- server/script.js
+++ server/script.js
@@ -5,34 +5,77 @@ const path = require("path");
 const process = require("process");
 console.log(path.join(__dirname, "data.json"));
 const dataPath = path.join(__dirname, "./data.json");
+const throat = require("throat");
 const [operation, amount, start] = process.argv.slice(2);
+
 const fetchTopPokemons = async (n, start = 1) => {
     const allPokemons = [];
+    const urls = [];
     for (let i = start; i <= n + start; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        axios
-            .get(url)
-            .then((res) => {
-                const { name, id, height, weight, types } = res.data;
-                const pokemon = {
-                    name,
-                    id,
-                    height,
-                    weight,
-                    img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
-                    types: types.map((typeSlot) => typeSlot.type.name),
-                };
-                console.log(
-                    `####       loaded pokemon ${name} id:${id} from: ${url}    ####`
-                );
-                allPokemons.push(pokemon);
-                fs.writeFileSync(dataPath, JSON.stringify(allPokemons));
-            })
-            .catch((err) => console.log(`###        ERROR: ${err}     ###`));
+        urls.push(`https://pokeapi.co/api/v2/pokemon/${i}`);
     }
+    const data = Promise.all(
+        urls.map(throat(10, (url) => fetchPokemon(url, allPokemons)))
+    );
     return allPokemons;
 };
 
+const fetchPokemon = (url, allPokemons) => {
+    axios
+        .get(url)
+        .then((res) => {
+            const { name, id, height, weight, types } = res.data;
+            const pokemon = {
+                name,
+                id,
+                height,
+                weight,
+                img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`,
+                types: types.map((typeSlot) => typeSlot.type.name),
+            };
+            console.log(
+                `####       loaded pokemon ${name} id:${id} from: ${url}    ####`
+            );
+            allPokemons.push(pokemon);
+            fs.writeFileSync(dataPath, JSON.stringify(allPokemons));
+        })
+        .catch((err) => console.log(`###        ERROR: ${err}     ###`));
+};
+
+const mix = () => {
+    const pokemons = JSON.parse(
+        fs.readFileSync(dataPath, { encoding: "utf-8" })
+    );
+    const pokefusion = [];
+    const maxId = pokemons.length + 1;
+    for (let i = 0; i < pokemons.length; i++) {
+        const firstPokemon = pokemons[i];
+        for (let j = i + 1; j < pokemons.length; j++) {
+            const secondPokemon = pokemons[j];
+            const newName =
+                firstPokemon.name.substr(
+                    0,
+                    parseInt(firstPokemon.name.length / 2)
+                ) +
+                secondPokemon.name.substr(
+                    parseInt(secondPokemon.name.length / 2),
+                    secondPokemon.name.length - 1
+                );
+            pokefusion.push({
+                id: maxId,
+                name: newName,
+                weight: firstPokemon.weight,
+                height: secondPokemon.height,
+                img: firstPokemon.img,
+                types: [],
+            });
+        }
+    }
+    fs.writeFileSync(
+        "./data.json",
+        JSON.stringify([...pokemons, ...pokefusion])
+    );
+};
 if (operation === "display") {
     const getAllIds = () => {
         const pokemons = JSON.parse(
@@ -55,4 +98,8 @@ if (operation === "fetch") {
         amount ? parseInt(amount) - 1 : 100,
         start ? parseInt(start) : 1
     );
+}
+
+if (operation === "mix") {
+    mix();
 }
